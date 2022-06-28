b05e8ac2abf38e260b4fae67902a914fc324effb
saurabh <999.sbpatel@gmail.com>
2022-06-29 03:58:29 +0530

    final commit

--- DATA_LAMBDA.sql
+++ DATA_LAMBDA.sql
@@ -1,4 +1,4 @@
 create database saurabh;
 use saurabh;
-create table Lambdaaa(username1 varchar(20),password1 varchar(20),email1 varchar(30),phone1 BIGINT);
+create table Lambdaaa(username1 varchar(30),password1 varchar(30),email1 varchar(30),phone1 BIGINT);
 select * from Lambdaaa;
